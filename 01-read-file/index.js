5f3b5ed6c456f1320399fba223770c2d469e5a8b
doritozbae <doritosbae@mail.ru>
2021-11-04 22:50:08 +0300

    feat: add solution

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');   
 const { stdout } = process;
 
-const  filePath = path.join(__dirname, 'text.txt');
+const filePath = path.join(__dirname, 'text.txt');
 let stream = fs.createReadStream(filePath, 'utf-8');
 
 let data = '';
