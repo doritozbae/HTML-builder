0d3a117494b96e696fae68bba428a4768edc2e38
doritozbae <doritosbae@mail.ru>
2021-11-08 23:24:15 +0300

    feat: add func for copying assets

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -7,6 +7,8 @@ const stylePath = path.join(__dirname, 'styles/');
 const componentsPath = path.join(__dirname, 'components/');
 const templateFiles = path.join(__dirname, 'template.html');
 
+const assetsPath = path.join(__dirname, 'assets/');
+const assetsProjectPath = path.join(projectPath, 'assets/');
 
 fs.mkdir(projectPath, { recursive: true }, err => {
    if (err) {
@@ -59,11 +61,49 @@ function buildCSS() {
 }
 buildCSS();
 
+
+const copyAssets = (assetsPath, assetsProjectPath) => {
+   fs.readdir(assetsPath, (err, files) => {
+   if (err) {
+         console.error(err);
+         return;
+      }
+
+     files.forEach(file => {
+      fs.stat(`${assetsPath}${file}`, (err, stats) => {
+         if (err) {
+            console.log(err);
+         } 
+         else {
+            if (stats.isDirectory()) {
+               // console.log('check')
+               return copyAssets(path.join(__dirname, `assets/${file}/`), path.join(__dirname, `project-dist/assets/${file}/`));
+            }
+            fs.copyFile(`${assetsPath}${file}`,
+               `${assetsProjectPath}${file}`, (err) => {
+                  if (err) {
+                     throw err;
+                  }
+               });
+         }
+      });
+   });
+   }
+   );
+
+   fs.mkdir(assetsProjectPath, { recursive: true }, err => {
+      if (err) {
+        throw err; 
+      }   
+  });
+};
+copyAssets(assetsPath, assetsProjectPath);
+
 // - [+] После завершения работы скрипта должна быть создана папка **project-dist**
 // - [+] В папке **project-dist** должны находиться файлы **index.html** и **stye.css** 
-// - [ ] В папке **project-dist** должна находиться папка **assets** являющаяся точной копией папки **assets** находящейся в **06-build-page**
+// - [+] В папке **project-dist** должна находиться папка **assets** являющаяся точной копией папки **assets** находящейся в **06-build-page**
 // - [+] Файл **index.html** должен содержать разметку являющуюся результатом замены шаблонных тегов в файле **template.html**
 // - [+] Файл **style.css** должен содержать стили собранные из файлов папки **styles** 
-// - [+-] При добавлении компонента в папку и соответствующего тега в исходный файл **template.html** повторное выполнение скрипта приведёт файл **index.html** в папке **project-dist** в актуальное состояние перезаписав его. Файл **style.css** и папка **assets** так же должны поддерживать актуальное состояние 
+// - [+] При добавлении компонента в папку и соответствующего тега в исходный файл **template.html** повторное выполнение скрипта приведёт файл **index.html** в папке **project-dist** в актуальное состояние перезаписав его. Файл **style.css** и папка **assets** так же должны поддерживать актуальное состояние 
 // - [+] Исходный файл **template.html** не должен быть изменён в ходе выполнения скрипта
 // - [+] Запись в шаблон содержимого любых файлов кроме файлов с расширением **.html** является ошибкой  
